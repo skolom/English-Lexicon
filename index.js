feee12b85a92d8c54146c4308059a62147154b87
skolom <nanaberimaoseiyaw@gmail.com>
2023-11-30 13:00:44 +0000

    Adding audio element

--- index.js
+++ index.js
@@ -3,46 +3,94 @@ const infoText = document.querySelector("#info-text");
 const meaningContainer = document.querySelector("#meaning-container");
 const title = document.querySelector("#title");
 const meaning = document.querySelector("#meaning");
+const audioEl = document.querySelector("#audio");
 
-async function fetchAPI(word) {
-
+ 
+async function fetchAPI(word) {  
+    
     try {
-
-        // infoText.style.display = "block";
-
-        // infoText.innerText = `Searching the meaning of "${word}"`      
-       
         
-        // const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
-        // const result = await fetch(url).then((res)=> res.json());
-
-        // infoText.style.display = "none";
-        // meaningContainer.style.display = "block";
-        // title.innerText = result[0].word;
-        // meaning.innerText = result[0].meanings[0].definitions[0].definition;
-
-        
-         //refactored response with axios
+    
+    
+    // refactored response with axios
         infoText.style.display = "block";
 
         infoText.innerText = `Searching the meaning of "${word}"`
 
-        const response = await axios.get((`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`), (res)=> res.json());        
+        const rsp = await axios.get((`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`));        
 
-        infoText.style.display = "none";
-        meaningContainer.style.display = "block";
+        if ((rsp.data[0].title)) {
 
-        return  title.innerText = response.data[0].word, meaning.innerText = response.data[0].meanings[0].definitions[0].definition;
-       
+            meaningContainer.style.display = "block";
+            infoText.style.display = "none";
+            title.innerText = word; 
+            meaning.innerText = "N/A";
+            audioEl.style.display = "none";
+            
+        } else {
+            
+            infoText.style.display = "none";
+            meaningContainer.style.display = "block";
+            audioEl.style.display = "inline-flex";    
+            title.innerText = rsp.data[0].word; 
+            meaning.innerText = rsp.data[0].meanings[0].definitions[0].definition;
+            audioEl.src = rsp.data[0].phonetics[0].audio;
+            
+        }
+
+        // infoText.style.display = "none";
+        // meaningContainer.style.display = "block";
+
+        // title.innerText = rsp.data[0].word, 
+        // meaning.innerText = rsp.data[0].meanings[0].definitions[0].definition;
+
+        
+
     } catch (error) {
         console.log(error);
+        infoText.innerText = `an error occured`;
     }
-      
-}
-
-inputEl.addEventListener("keyup", (e)=>{
-    if (e.target.value && e.key === "Enter") {
-        fetchAPI(e.target.value)
         
     }
-})
+    
+
+    
+    inputEl.addEventListener("keyup", (e)=>{
+        if (e.target.value && e.key === "Enter") {
+            fetchAPI(e.target.value)
+            
+        }
+    })
+
+ 
+
+
+// async function fetchAPI(word) {
+
+//     try {
+
+//         infoText.style.display = "block";
+
+//         infoText.innerText = `Searching the meaning of "${word}"`      
+       
+        
+//         const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+//         const result = await axios.get((url).then((res)=> res.json()));
+
+//         infoText.style.display = "none";
+//         meaningContainer.style.display = "block";
+//         title.innerText = result[0].word;
+//         meaning.innerText = result[0].meanings[0].definitions[0].definition;
+
+//     } catch (error) {
+//         console.log(error); 
+//     }
+      
+//         inputEl.addEventListener("keyup", (e)=>{
+//             if (e.target.value && e.key === "Enter") {
+//                 fetchAPI(e.target.value)
+                
+//             }
+//         })
+//     }    
+
